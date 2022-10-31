cadc41815045baa9b46cf1b08c2680a081a47627
yourgotocoder <yourgotocoder@gmail.com>
2022-10-31 16:52:49 +0530

    Clean data ready

---
+++ helpers/cleaner.js
@@ -0,0 +1,56 @@
+const parser = require("convert-excel-to-json");
+const json2xls = require("json2xls");
+const fs = require("fs");
+
+const dateIsValid = (date) => {
+  return date instanceof Date && !isNaN(date);
+};
+
+const alumniData = [
+  ...parser({
+    sourceFile: "./resources/CSE_Alumni_List.xlsx",
+    header: { rows: 1 },
+  })["Members-6300-20-10-2022"],
+];
+
+const transformedAlumniData = alumniData.map((alumni) => {
+  const degrees = alumni.E.split(";");
+  return {
+    Salutation: alumni.A.trim(),
+    Name: alumni.B.toUpperCase(),
+    Gender: alumni.C,
+    DateOfBirth: new Date(alumni.D),
+    BTech: alumni.E.split(";")[0],
+    ...(degrees[1] !== undefined
+      ? degrees[0].trim() !== degrees[1].trim()
+        ? {
+            MTech: alumni.E.split(";")[1].trim(),
+          }
+        : { MTech: "" }
+      : { MTech: "" }),
+    EmailId: alumni.F.trim(),
+  };
+});
+
+const filterForInvalidDate = transformedAlumniData.filter(
+  (alumni) => !dateIsValid(new Date(alumni.DateOfBirth))
+);
+const filterForValidDate = transformedAlumniData.filter((alumni) =>
+  dateIsValid(new Date(alumni.DateOfBirth))
+);
+
+const xlsValidData = json2xls(filterForValidDate);
+const xlsInvalidData = json2xls(filterForInvalidDate);
+
+fs.writeFileSync(
+  "./resources/CSE_Alumni_Clean_Data.xlsx",
+  xlsValidData,
+  "binary"
+);
+fs.writeFileSync(
+  "./resources/CSE_Alumni_Invalid_Data.xlsx",
+  xlsInvalidData,
+  "binary"
+);
+
+// console.log(transformedAlumniData);
