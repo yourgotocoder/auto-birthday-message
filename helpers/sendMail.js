c3282cfc4907891cbdc8f538f2c9d5086e827d9d
yourgotocoder <yourgotocoder@gmail.com>
2022-11-01 15:19:20 +0530

    Map of birthdays done

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -1,23 +1,28 @@
 const outlook = require("nodejs-nodemailer-outlook");
 const emailLogger = require("./emailLogger");
-
-const htmlContent = ``;
+const numberToWordsMap = require("./numberToWordsMap");
 
 const sendMail = (config) => {
-  outlook.sendEmail({
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-    from: process.env.EMAIL_USER,
-    to: config.emailTo,
-    subject: config.emailSubject,
-    html: htmlContent,
-    text: "Happy Birthday",
-    replyTo: "noreply@mail.com",
-    onError: (e) => emailLogger("error", e),
-    onSuccess: (successData) => emailLogger("success", successData),
-  });
+  const currentAge =
+    new Date().getFullYear() - new Date(config.DateOfBirth).getFullYear();
+  const htmlContent = ``;
+  console.log(
+    `Happy ${currentAge}${numberToWordsMap[currentAge] || "th"} Birthday`
+  );
+  //   outlook.sendEmail({
+  //     auth: {
+  //       user: process.env.EMAIL_USER,
+  //       pass: process.env.EMAIL_PASS,
+  //     },
+  //     from: process.env.EMAIL_USER,
+  //     to: config.EmailId,
+  //     subject: `Happy ${currentAge}${numberToWordsMap[currentAge] || 'th'}Birthday`,
+  //     html: htmlContent,
+  //     text: "Happy Birthday",
+  //     replyTo: "noreply@mail.com",
+  //     onError: (e) => emailLogger("error", e),
+  //     onSuccess: (successData) => emailLogger("success", successData),
+  //   });
 };
 
 module.exports = sendMail;
--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 const cron = require("node-cron");
-const parser = require("simple-excel-to-json");
 const mailer = require("./helpers/sendMail");
-const fs = require("fs");
-
+const alumniData = require("./resources/CSE_Alumni.json");
 
 require("dotenv").config();
-const alumniData = [
-    ...parser.parseXls2Json("./resources/CSE_Alumni_Data.xlsx")[0],
-];
-fs.writeFileSync("./resources/CSE_Alumni.json", JSON.stringify(alumniData));
+
+// console.log(alumniData)
+const date = new Date();
+const month = date.getMonth() + 1;
+const day = date.getDate();
+const alumniWithBirthdayToday = alumniData.filter((alumni) => {
+  const alumniBirthday = new Date(alumni.DateOfBirth);
+  const alumniMonth = alumniBirthday.getMonth() + 1;
+  const alumniDay = alumniBirthday.getDate();
+  if (month === alumniMonth && day === alumniDay) {
+    return true;
+  } else return false;
+});
+
+for (let index = 0; index < alumniWithBirthdayToday.length; index++) {
+  setTimeout(() => {    
+    mailer({...alumniWithBirthdayToday[index]})
+  }, 2100 * (index + 1));
+}
 
 // const task = cron.schedule("*/5 * * * * *", () => {
-//   const date = new Date();
-//   const month = date.getMonth() + 1;
-//   const day = date.getDate();
-//   const alumniWithBirthdayToday = alumniData.filter((alumni) => {
-//     const alumniBirthday = new Date(alumni.DateOfBirth);
-//     const alumniMonth = alumniBirthday.getMonth() + 1;
-//     const alumniDay = alumniBirthday.getDate();
-//     if (alumni.Name === "DEVYADUTI  ROY") {
-//       return true;
-//     } else return false;
-//   });
-//   console.log(alumniWithBirthdayToday);
 // });
 
 // task.start();
