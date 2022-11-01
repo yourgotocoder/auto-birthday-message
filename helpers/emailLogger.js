f6dde3021b2e5262daab1eed0179aba30a10a1ec
yourgotocoder <yourgotocoder@gmail.com>
2022-11-01 11:48:33 +0530

    Fixed error and success logs

--- helpers/emailLogger.js
+++ helpers/emailLogger.js
@@ -2,9 +2,9 @@ const fs = require("fs");
 
 const emailLogger = (typeOfLog, logData) => {
   if (typeOfLog === "error") {
-    fs.appendFileSync("./Log/ErrorLog.txt", logData + "\n");
+    fs.appendFileSync("./log/ErrorLog.txt", new Date() + ": " + logData + "\n");
   } else if (typeOfLog === "success") {
-    fs.appendFileSync("./Log/SuccessLog.txt", logData + "\n");
+    fs.appendFileSync("./log/SuccessLog.txt", new Date() + ": " + logData + "\n");
   }
 };
 
--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -1,25 +1,23 @@
 const outlook = require("nodejs-nodemailer-outlook");
+const emailLogger = require("./emailLogger");
 
 const htmlContent = ``;
 
 const sendMail = (config) => {
-    outlook.sendEmail({
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        },
-        from: process.env.EMAIL_USER,
-        to: config.emailTo,
-        subject: config.emailSubject,
-        html: htmlContent,
-        text: 'Happy Birthday',
-        replyTo: 'noreply@mail.com',
-        onError: (e) => console.log(e),
-        onSuccess: (i) => console.log(i)
-    }
-    
-    
-    );
-}
+  outlook.sendEmail({
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+    from: process.env.EMAIL_USER,
+    to: config.emailTo,
+    subject: config.emailSubject,
+    html: htmlContent,
+    text: "Happy Birthday",
+    replyTo: "noreply@mail.com",
+    onError: (e) => emailLogger("error", e),
+    onSuccess: (i) => console.log(i),
+  });
+};
 
 module.exports = sendMail;
