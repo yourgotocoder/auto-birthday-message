5c3c685ec7ce3e8a8b42e8bad8a07dc09fd6e5c3
yourgotocoder <yourgotocoder@gmail.com>
2022-11-04 16:01:36 +0530

    Added Birthday Messages

---
+++ helpers/birthdayMessages.js
@@ -0,0 +1,13 @@
+const birthdayMessages = [
+  "Wishing you a great birthday and a memorable year.",
+  "Hoping your birthday brings you many happy reasons to celebrate!.",
+  "Wishing you the best on your birthday and everything good in the year ahead.",
+  "Hope your day is filled with happiness.",
+  "Wishing you a happy birthday and a wonderful year.",
+  "Our whole team is wishing you the happiest of birthdays.",
+  "Happy Birthday and all the best to you in the year to come!.",
+  "Wishing you a relaxing birthday and joy and happiness in the year to come.",
+  "The whole team wishes you the happiest of birthdays and a great year.",
+];
+
+module.exports = birthdayMessages;
