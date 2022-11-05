c4232b3b7f85de985a82607c57c5e7c6b2575aeb
yourgotocoder <yourgotocoder@gmail.com>
2022-11-05 10:11:39 +0530

    Final script ready v1.0.1

--- index.js
+++ index.js
@@ -6,23 +6,21 @@ const cron = require("node-cron");
 require("dotenv").config();
 
 cron.schedule("0 4 * * *", () => {
-	const date = new Date();
-	const month = date.getMonth() + 1;
-	const day = date.getDate();
-	const alumniWithBirthdayToday = alumniData.filter((alumni) => {
-  	const alumniBirthday = new Date(alumni.DateOfBirth);
-  	const alumniMonth = alumniBirthday.getMonth() + 1;
-  	const alumniDay = alumniBirthday.getDate();
-  		if (month === alumniMonth && day === alumniDay) {
-   			return true;
-  		} else return false;
-	});
+  const date = new Date();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const alumniWithBirthdayToday = alumniData.filter((alumni) => {
+    const alumniBirthday = new Date(alumni.DateOfBirth);
+    const alumniMonth = alumniBirthday.getMonth() + 1;
+    const alumniDay = alumniBirthday.getDate();
+    if (month === alumniMonth && day === alumniDay) {
+      return true;
+    } else return false;
+  });
 
-	for (let index = 0; index < alumniWithBirthdayToday.length; index++) {
-  		setTimeout(() => {
-    			mailer({...alumniWithBirthdayToday[index]})
-  	}, 2100 * (index + 1));
-	}
-
+  for (let index = 0; index < alumniWithBirthdayToday.length; index++) {
+    setTimeout(() => {
+      mailer({ ...alumniWithBirthdayToday[index] });
+    }, 2100 * (index + 1));
+  }
 });
-
